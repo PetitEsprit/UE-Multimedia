7a62a00c3391c7ef7fe30e8f64e1831b11141191
PetitEsprit <PetitEsprit@users.noreply.github.com>
2023-03-19 19:32:14 +0100

    add summary, get_occur and now token is dict

--- info_search/tok.py
+++ info_search/tok.py
@@ -1,11 +1,29 @@
-class Token:
-	"""Token class"""
-	def __init__(self, content, count):
+import json
+from typing import Dict, List
+
+Doc = Dict
+
+"""class Token:
+	def __init__(self, content, count=1):
 		self.content = content
 		self.count = count
+	def __str__(self):
+		return f"({self.content}: {self.count})"
+	def __repr__(self):
+   		return self.__str__()
+"""
 
 seps = ['\n', '\t', '\v', '\f', '\r', ' ', #spaces
-		'.', ',',';', ":", '!', '?'] #punctuation
+		'.', ',',';', ":", '!', '?', #punctuation
+		'\'', '\`', '\"'
+	]
+
+def load_stopwords(path) -> list:
+	l = []
+	with open(path) as f:
+		l = f.read()
+	l = l.split()
+	return l
 
 def tokenize(path) -> list:
 	l = []
@@ -16,29 +34,58 @@ def tokenize(path) -> list:
 		txt = f.read()
 
 	while i < len(txt):
-		print(txt[i], i)
+		#print(txt[i], i)
 		if (start < i and txt[i] in seps):
-			l.append(txt[start:i])
+			l.append(txt[start:i].lower())
 			start = i = i + 1
 		while (i < len(txt) and txt[i] in seps):
 			start = i = i + 1
 		i = i + 1
 	if (start < i):
-		l.append(txt[start:i])
+		l.append(txt[start:i].lower())
 	l = [item.strip() for item in l]
 	return l
 
-#def load_list_token(path) -> list:
-#	
+def load_list_token(path, stpwpath) -> Doc:
+	l = tokenize(path)
+	stopwords = load_stopwords(stpwpath)
+	uniq = []
+	[uniq.append(u) for u in l if u not in uniq if u not in stopwords
+		if len(u) > 2 #additional rule
+	]
+	#toks = [Token(u, l.count(u)) for u in uniq]
+	toks = [{"str": u, "count": l.count(u)} for u in uniq]
+	toks.sort(reverse=True, key=lambda token: token["count"])
+	doc = {"name": path, "words": toks}
+	return doc
 
-def load_stopwords(path) -> list:
+
+
+def summary(*argv: Doc):
+	#Should take doc or list_doc and print summary
+	for arg in argv:
+		print(f'===Document: {arg["name"]}===')
+		total_count = 0
+		for t in arg["words"]:
+			total_count = total_count + t["count"]
+		for t in arg["words"]:
+			print(f'{t["content"]} -  freq: ', "{:.2f}".format(t["count"]/total_count))
+
+def get_occur(word: str, *docs: Doc) -> list[tuple[str, int]]:
 	l = []
-	with open(path) as f:
-		l = f.read()
-	l = l.split()
+	for d in docs:
+		for w in d["words"]:
+			if w["str"] == word:
+				l.append((d["name"], w["count"]))
+	l.sort(reverse=True, key=lambda occur: occur[1])
 	return l
 
-toks = tokenize("asimov.txt")
-print(toks)
+doc = load_list_token("asimov.txt", "stopwords.txt")
+occurs = get_occur("robot", doc)
+print(occurs)
+#print(toks)
+#summary(toks)
+#print(json.dumps(toks, indent=4))
+#print(x.keys())
 #print(load_stopwords("stopwords.txt"))
 #print(str(Token("fefez", 1)))
